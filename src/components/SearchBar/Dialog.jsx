be9d83f6b7aa972cf969f5e7b08f6df56a08fbdb
iamBijoyKar <bijoypoco12345@gmail.com>
2023-11-12 21:28:13 +0530

    [ADD]: search clear feature added

--- src/components/SearchBar/Dialog.jsx
+++ src/components/SearchBar/Dialog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Dialog, DialogActions, Button } from '@mui/material';
 import { useHotkeys } from 'react-hotkeys-hook';
+import { AiOutlineClose } from 'react-icons/ai';
 
 SimpleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
@@ -18,6 +19,10 @@ export default function SimpleDialog(props) {
     onClose();
   };
 
+  const handleClear = () => {
+    setSearch('');
+  };
+
   // hotkeys for opening and closing the dialog
   useHotkeys('ctrl+k', () => {
     onOpen();
@@ -29,7 +34,7 @@ export default function SimpleDialog(props) {
 
   return (
     <Dialog onClose={handleClose} open={open} fullWidth>
-      <div className="p-4 rounded-lg flex gap-2 items-center  ">
+      <div className="p-4 rounded-lg flex gap-2 items-center w-full relative  ">
         <input
           autoFocus
           onChange={(e) => setSearch(e.target.value)}
@@ -38,6 +43,12 @@ export default function SimpleDialog(props) {
           className="w-full text-md p-2 rounded-lg bg-slate-200 text-slate-800 outline-slate-400"
           placeholder="Quick Search..."
         />
+        {search.trim() !== '' && (
+          <AiOutlineClose
+            className="text-2xl text-slate-800 absolute  right-20 cursor-pointer hover:text-slate-700"
+            onClick={handleClear}
+          />
+        )}
         <span className="px-2 py-1 rounded-lg bg-slate-500 text-white">
           Esc
         </span>
