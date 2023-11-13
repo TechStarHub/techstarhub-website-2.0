85ddc335b212de93e8c2e1d80d06b45c48e300de
iamBijoyKar <bijoypoco12345@gmail.com>
2023-11-13 21:44:44 +0530

    [UPDATE]: hero animation added

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import { Typography, Button } from '@mui/material';
-import { useAnimate, useInView, motion } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+import { Typography } from '@mui/material';
+import { useAnimate, useInView, animate } from 'framer-motion';
 
 export default function Hero() {
   const windowWidth = window.innerWidth;
@@ -10,6 +10,10 @@ export default function Hero() {
   const [heroScope, heroAnimate] = useAnimate();
   const isHeroInView = useInView(heroScope);
 
+  const text1 = useRef(null);
+  const text2 = useRef(null);
+  const text3 = useRef(null);
+
   useEffect(() => {
     if (isHeroInView) {
       heroAnimate(
@@ -18,41 +22,92 @@ export default function Hero() {
           opacity: 1,
         },
         {
-          duration: 1,
+          duration: 0.3,
+        },
+      );
+      animate(
+        text1.current,
+        {
+          opacity: 1,
+          y: 0,
+        },
+        {
+          duration: 0.7,
+        },
+      );
+      animate(
+        text2.current,
+        {
+          opacity: 1,
+          y: 0,
+        },
+        {
+          duration: 0.7,
+          delay: 0.5,
+        },
+      );
+      animate(
+        text3.current,
+        {
+          opacity: 1,
+          y: 0,
+        },
+        {
+          duration: 0.7,
+          delay: 1,
         },
       );
     } else {
       heroAnimate(heroScope.current, {
         opacity: 0,
       });
+      animate(
+        text1.current,
+        {
+          opacity: 0,
+          y: -windowHeight,
+        },
+        {
+          duration: 1,
+        },
+      );
+      animate(
+        text2.current,
+        {
+          opacity: 0,
+          y: -windowHeight,
+        },
+        {
+          duration: 1,
+        },
+      );
+      animate(text3.current, {
+        opacity: 0,
+        y: -windowHeight,
+      });
     }
   }, [isHeroInView]);
+
   return (
     <div
       ref={heroScope}
       className="flex flex-col items-center justify-center min-h-[90vh] bg-[url('/hero-bg.jpg')] bg-center bg-cover "
     >
-      <motion.h1
-        initial={{ opacity: 0, y: -windowHeight }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.7, delay: 0.5, ease: 'easeInOut' }}
+      <h1
+        ref={text1}
         className="text-white text-5xl md:text-[6rem] font-bold  "
       >
         TechStarHub
-      </motion.h1>
-      <motion.h4
-        initial={{ opacity: 0, y: -windowHeight }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.7, delay: 0.7, ease: 'easeInOut' }}
+      </h1>
+      <h4
+        ref={text2}
         className="text-white text-2xl md:text-[2rem] font-bold mt-4 "
       >
         Learn | Build | Share | Connect
-      </motion.h4>
+      </h4>
       <div className="flex gap-4 mt-4">
-        <motion.button
-          initial={{ opacity: 0, y: -windowHeight }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7, delay: 1, ease: 'easeInOut' }}
+        <button
+          ref={text3}
           className="group rounded-full border-white border-[2px] px-6 py-2 hover:bg-white shadow "
         >
           <Typography
@@ -61,7 +116,7 @@ export default function Hero() {
           >
             Join Us
           </Typography>
-        </motion.button>
+        </button>
       </div>
     </div>
   );
