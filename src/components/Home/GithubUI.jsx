943f353bef8d0dda66c684b8b001f72999361f26
iamBijoyKar <bijoypoco12345@gmail.com>
2023-11-14 15:11:57 +0530

    [ADD]: open source section added

---
+++ src/components/Home/GithubUI.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { BsGithub, BsArrowLeftShort } from 'react-icons/bs';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { Avatar } from '@mui/material';
+import { TbGitCompare } from 'react-icons/tb';
+import { TiTick } from 'react-icons/ti';
+import { DiGitCommit } from 'react-icons/di';
+import { BsFileEarmarkDiff } from 'react-icons/bs';
+import { PiUsersBold } from 'react-icons/pi';
+import { FaMousePointer } from 'react-icons/fa';
+import { GoCopy } from 'react-icons/go';
+import { FiCode } from 'react-icons/fi';
+
+export default function GithubUI() {
+  const date = new Date();
+  const dateStr = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+  return (
+    <div className="w-full bg-[] relative">
+      <motion.span
+        animate={{ y: [0, 100, 170, 170, 170], x: [0, 100, 400, 400, 400] }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: 'reverse',
+          ease: 'linear',
+        }}
+        className="absolute top-5 left-4 z-50"
+      >
+        <FaMousePointer className="text-2xl text-slate-100 " />
+      </motion.span>
+      <div className="flex justify-between items-center gap-3 w-full bg-[#010409] px-3 py-2">
+        <div className="flex items-center gap-3">
+          <span className="border border-slate-400 rounded p-[4px] cursor-pointer hover:border-slate-300">
+            <AiOutlineMenu className="text-xl text-slate-400" />
+          </span>
+          <BsGithub className="text-3xl text-slate-100" />
+          <span className="text-white">
+            TechStarHub / <span className="font-medium">react</span>
+          </span>
+        </div>
+        <span className=" cursor-pointer">
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            sizes="sm"
+            src="/identicon.png"
+            className="  "
+          />
+        </span>
+      </div>
+      <div className="w-full bg-[#0d1117] p-3">
+        <h4 className="text-lg text-white">Comparing changes</h4>
+        <p className="text-sm text-slate-400">
+          Choose two branches to see what’s changed or to start a new pull
+          request.
+        </p>
+        <div className=" w-full flex items-center bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-600 py-1 px-2 my-2">
+          <TbGitCompare className="text-xl text-slate-400 mr-2" />
+          <span className="text-slate-400 px-1 bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-700">
+            base:<span className="text-white">main</span>
+          </span>
+          <BsArrowLeftShort className="text-xl text-slate-400" />
+          <span className="text-slate-400 px-1 bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-700">
+            compare:<span className="text-white">feature</span>
+          </span>
+          <span className="text-[#239150] font-medium ml-3">
+            <TiTick className="text-2xl inline-block" />
+            Able to merge.
+          </span>
+        </div>
+        <div className="bg-[#537FE7] rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-[#537FE7] flex items-center justify-between px-2 py-1">
+          <span className="text-white text-[12px]">
+            Discuss and review the changes in this comparison with others.
+          </span>
+          <span className="w-[9rem] h-[30px] px-2 p-1 rounded-md text-white text-sm cursor-pointer bg-[#239150] hover:bg-[#2ea043]">
+            Create pull request
+          </span>
+        </div>
+        <div className="w-full px-2 py-2 border border-slate-400 mt-3 rounded flex justify-around">
+          <span className="text-sm text-slate-400">
+            <DiGitCommit className="inline-block mr-1 text-xl" />
+            <span className="text-white mr-1">1</span>
+            commits
+          </span>
+          <span className="text-sm text-slate-400">
+            <BsFileEarmarkDiff className="inline-block mr-1 text-xl" />
+            <span className="text-white mr-1">6</span>
+            files changed
+          </span>
+          <span className="text-sm text-slate-400">
+            <PiUsersBold className="inline-block mr-1 text-xl" />
+            <span className="text-white mr-1">3</span>
+            contributors
+          </span>
+        </div>
+        <div className="w-full mt-3">
+          <DiGitCommit className="inline-block mr-1 text-xl text-slate-400" />
+          <span className="text-slate-400">Commits on {dateStr}</span>
+          <div className="w-full mt-2 border border-slate-300 rounded-md">
+            <div className="px-3 py-2">
+              <div className="flex items-start justify-between">
+                <span className="text-white hover:underline hover:text-[#1b66f7] cursor-pointer ">
+                  [ADD]: requested feature added
+                </span>
+                <div className="flex items-center gap-3">
+                  <div className="bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 flex items-center p-0">
+                    <span className="p-1 cursor-pointer">
+                      <GoCopy className="text-xl text-slate-400" />
+                    </span>
+                    <span className="text-sm border-l p-1 pl-2 cursor-pointer">
+                      ebdgg56
+                    </span>
+                  </div>
+                  <div className="bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 p-1 cursor-pointer">
+                    <FiCode className="text-xl text-slate-400" />
+                  </div>
+                </div>
+              </div>
+              <span className="flex items-center gap-2 text-sm">
+                <Avatar
+                  sx={{ width: 20, height: 20 }}
+                  sizes="sm"
+                  src="/identicon.png"
+                  className="cursor-pointer"
+                />
+                <span className="text-white">user</span>
+                <span className="text-slate-400">commited 2 hours ago</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Home/OpenSource.jsx
+++ src/components/Home/OpenSource.jsx
@@ -1,7 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
+import wave from '../../assets/wave.svg';
+import githubIcon from '../../assets/github.svg';
+import gitIcon from '../../assets/git.svg';
+import gitlabIcon from '../../assets/gitlab.svg';
+import { SiGnubash } from 'react-icons/si';
+import { BsGithub } from 'react-icons/bs';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { anOldHope } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import GithubUI from './GithubUI';
+
+const code = `user@device MINGW64 /d/Projects (main) $ git add .
+user@device MINGW64 /d/Projects (main) $ git status
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   main.js
+
+user@device MINGW64 /d/Projects (main) $ git commit -m "Added a new feature"
+user@device MINGW64 /d/Projects (main) $ git pull origin main
+user@device MINGW64 /d/Projects (main) $ git push origin main
+`;
 
 export default function OpenSource() {
+  const mode = useSelector((state) => state.mode.mode);
+  const isDark = mode === 'dark' ? true : false;
+
+  const [codeIdx, setCodeIdx] = useState(0);
+
   return (
-    <div className="w-full flex justify-center items-center min-h-screen"></div>
+    <div
+      className="w-full flex flex-col justify-center items-center min-h-screen relative py-10"
+      style={{
+        backgroundColor: isDark ? '#213555' : '#F0F0F0',
+        color: isDark ? '#ffffff' : '#000000',
+      }}
+    >
+      <img src={wave} alt="wave" className="absolute top-0 left-0 w-full" />
+
+      <div className="w-full px-4 md:px-[9vw] mb-4 z-10">
+        <h1 className="text-xl font-bold uppercase text-[#191D88]">
+          Open Source Contribution
+        </h1>
+        <p className="text-3xl ">
+          Make your first contribution to open source.
+        </p>
+      </div>
+
+      <div className="w-full h-full flex flex-col md:flex-row justify-center items-center z-10">
+        <div className=" w-[80vw] md:w-[40vw] flex ">
+          <motion.img
+            animate={{
+              x: [0, 160, 0, 0],
+              y: [0, 100, 200, 0],
+              scale: [1, 1.1, 1.3, 1],
+            }}
+            style={{ filter: 'drop-shadow( 0 0 5px #00000055)' }}
+            transition={{ duration: 3, repeat: Infinity, ease: 'linear' }}
+            src={githubIcon}
+            className="w-full"
+            alt=""
+          />
+          <motion.img
+            animate={{
+              x: [-100, 0, 0, -100],
+              y: [100, 160, 0, 100],
+              scale: [1, 1.1, 1.3, 1],
+            }}
+            transition={{ duration: 3, repeat: Infinity, ease: 'linear' }}
+            src={gitIcon}
+            style={{ filter: 'drop-shadow( 0 0 5px #F94A2955)' }}
+            className="w-full"
+            alt=""
+          />
+          <motion.img
+            animate={{
+              x: [0, -160, 0, 0],
+              y: [0, 100, 200, 0],
+              scale: [1, 1.1, 1.3, 1],
+            }}
+            transition={{ duration: 3, repeat: Infinity, ease: 'linear' }}
+            src={gitlabIcon}
+            style={{ filter: 'drop-shadow( 0 0 5px #F94A2955)' }}
+            className="w-full"
+            alt=""
+          />
+        </div>
+        <div className="w-[100vw] md:w-[40vw] h-full">
+          <div className="h-full w-full p-4 bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 flex flex-col gap-1">
+            <div className="flex w-full">
+              <span
+                style={{
+                  border: codeIdx == 0 && '0 0 1px 0 #fff',
+                  borderBottom: codeIdx == 0 && '2px solid #00FFCA',
+                }}
+                onClick={() => setCodeIdx(0)}
+                className="text-md cursor-pointer rounded hover:bg-slate-600 px-2 py-1"
+              >
+                <SiGnubash className="inline-block mr-2" />
+                <span className="mr-2">bash</span>
+              </span>
+              <span
+                style={{
+                  border: codeIdx == 1 && '0 0 1px 0 #fff',
+                  borderBottom: codeIdx == 1 && '2px solid #181823',
+                }}
+                onClick={() => setCodeIdx(1)}
+                className="text-md cursor-pointer rounded hover:bg-slate-600 px-2 py-1"
+              >
+                <BsGithub className="inline-block mr-2" />
+                <span className="mr-2">GitHub</span>
+              </span>
+            </div>
+            {codeIdx === 0 && (
+              <SyntaxHighlighter
+                language="bash"
+                style={anOldHope}
+                className="rounded-md"
+              >
+                {code}
+              </SyntaxHighlighter>
+            )}
+            {codeIdx === 1 && (
+              <div className="">
+                <GithubUI />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
